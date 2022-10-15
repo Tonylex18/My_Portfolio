66927f1f8c3b1c9ec873fd461612b39885e59e52
Tonylex18 <anthonyagada2000@gmail.com>
2022-10-15 16:22:09 +0100

    front end

---
+++ inc/about.php
@@ -0,0 +1,26 @@
+<section class="about" id="about">
+	<div class="about-img">
+		<img src="Imgs/about-me.jpg">
+	</div>
+
+	<div class="about-text">
+		<h2>About Me</h2>
+		<h4>Hi, I'm here to help your next project</h4>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut eaque, laboriosam veritatis, quos non quis ad perspiciatis, totam corporis ea, alias ut unde dolor sit amet, consectetur adipisicing elit. Aut eaque, laboriosam veritatis, quos non quis ad perspiciatis, totam corporis ea, alias ut unde</p>
+		<div class="about-gri">
+			<div class="about-in">
+				<h5>1. Problem Solving</h5>
+			</div>
+			<div class="about-in">
+				<h5>2. Creative Ideas</h5>
+			</div>
+			<div class="about-in">
+				<h5>3. Search Alot</h5>
+			</div>
+			<div class="about-in">
+				<h5>4. High Quality</h5>
+			</div>
+		</div>
+		<a href="#contact" class="btn">Contact Me</a>
+	</div>
+</section>
--- js/script.js
+++ js/script.js
@@ -2,13 +2,23 @@ let btn = document.querySelector(".toggle");
 let icon = btn.querySelector(".fa-bars");
 let navbar = document.querySelector ('.navbar');
 
-btn.onclick = function() {
+btn.onclick = function(){
 	if(icon.classList.contains("fa-bars")){
-		icon.classList.replace("fa-bars", "fa-times");
-		navbar.classList.toggle('active');
-	}
-	esle {
-		icon.classList.replace("fa-times", "fa-bars");
-		navbar.classList.remove('active');
-	}
+	icon.classList.replace("fa-bars", "fa-times");
+	navbar.classList.toggle('active');
 }
+else {
+	icon.classList.replace("fa-times", "fa-bars");
+	navbar.classList.remove('active');
+	}
+};
+
+window.onscroll = function(){
+	if(icon.classList.contains("fa-bars")){
+	navbar.classList.remove('active');
+}
+else {
+	icon.classList.replace("fa-times", "fa-bars");
+	navbar.classList.remove('active');
+	}
+};
