d2d231f26f683afdf6eb964fa1ca49c3158becc6
Tonylex18 <anthonyagada2000@gmail.com>
2022-10-19 12:50:08 +0100

    front end

--- inc/home.php
+++
@@ -1,8 +0,0 @@
-<section class="home" id="home">
-	<div class="home-text">
-		<h4>Hello</h4>
-		<h1>I'm Anthony <br><span>Alex</span></h1>
-		<h3>Front-End Developer</h3>
-		<a href="#contact" class="btn">Hire Me</a>
-	</div>
-</section>
